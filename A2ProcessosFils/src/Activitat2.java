bc1359766b09db25f001b4666d2e3a1e150bbef0
Tiffany <tiffany.fernandez@insbaixcamp.cat>
2019-10-24 18:12:54 +0200

    exercici acabat

---
+++ A2ProcessosFils/src/Activitat2.java
@@ -0,0 +1,44 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Activitat2 {
+
+    public static void main(final String... args) throws InterruptedException, ExecutionException {
+        //mostrem hora actual abans d’execució
+    	Calendar calendario = new GregorianCalendar();   
+    	System.out.println("Inici: "+ calendario.get(Calendar.HOUR_OF_DAY) + ":" + calendario.get(Calendar.MINUTE) +
+        ":" + calendario.get(Calendar.SECOND));
+	    // Crea un pool de 4 fils
+	    final ScheduledExecutorService schExService = Executors.newScheduledThreadPool(4);
+	    // Crea objecte Runnable.
+	    final Runnable ob = new Activitat2().new ExecutaFil();
+	    //Es realitza 5 tasques
+	    for (int i = 0; i < 5; i++){
+	    // Programa Fil, s’inicia als 5 segons i després es va executant cada 6 segons
+	    schExService.scheduleWithFixedDelay(ob, 5, 6, TimeUnit.SECONDS);
+	    }
+	    // Espera per acabar 5 segons
+	    schExService.awaitTermination(5, TimeUnit.SECONDS);
+	    // shutdown .
+	    schExService.shutdownNow();
+	    System.out.println("Completat");
+    }
+
+    // Fil Runnable
+    class ExecutaFil implements Runnable {
+        @Override
+        public void run() {
+            Calendar calendario = new GregorianCalendar();
+            System.out.println("Hora execució tasca: "+
+                calendario.get(Calendar.HOUR_OF_DAY) + ":" +
+                calendario.get(Calendar.MINUTE) + ":" +
+                calendario.get(Calendar.SECOND));
+            System.out.println("Tasca en execució");
+            System.out.println("Execució acabada");
+            }
+        }
+    }
